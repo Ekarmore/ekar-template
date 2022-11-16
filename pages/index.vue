1d8506f53d8f49d17efcaa1a283fe06c9125e179
EKar <Earthb0y@outlook.com>
2022-11-16 21:38:05 +0800

    build page

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Suspense>
+      <PageView />
+      <template #fallback>
+        <div op50 italic>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <div class="one">
+      111
+    </div>
+  </div>
+</template>
+
+<style>
+.one{
+@apply text-6xl font-bold
+}
+</style>
