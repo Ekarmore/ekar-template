56e92dd0eb2b27a11d70fd6c6c3506275a5503e0
EKar <Earthb0y@outlook.com>
2022-11-16 21:46:10 +0800

    build page

---
+++ components/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="zutest">
+    组件TEST
+  </div>
+</template>
+
+<style>
+.zutest{
+@apply text-6xl text-blue-600 font-mono
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@
     <div class="one">
       TEST
     </div>
+    <test />
   </div>
 </template>
 
