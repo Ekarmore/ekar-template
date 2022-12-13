020cd7ea71fa0acc8c8c7768e4a7dc05b9d2c86a
EKar <Earthb0y@outlook.com>
2022-12-13 15:25:10 +0800

    page build

--- utils/pageAnimate.ts
+++ utils/pageAnimate.ts
@@ -4,8 +4,7 @@ const tl = gsap.timeline()
 export function imagesAnimate() {
   tl.from('#img-wrapper', {
     autoAlpha: 0,
-    filter: 'blur-90',
-    translateY: 50,
+    translateY: 30,
     duration: 0.7,
   })
 }
@@ -13,8 +12,7 @@ export function imagesAnimate() {
 export function aboutAnimate() {
   tl.from('#about-wrapper', {
     autoAlpha: 0,
-    filter: 'blur-90',
-    translateY: 50,
+    translateY: 30,
     duration: 0.7,
   })
 }
