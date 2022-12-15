6756934bef2d47e6abd9bad57e789f7e5d1f1ae5
EKar <Earthb0y@outlook.com>
2022-12-15 10:17:48 +0800

    page build

--- pages/[...pageNotFound].vue
+++ pages/[...pageNotFound].vue
@@ -3,12 +3,9 @@
 const router = useRouter()
 const route = useRoute()
 const { pageNotFound } = route.params
-onMounted(() => {
-  NotFoundAnimate()
-})
 useHead(
   {
-    title: `404 not found ${pageNotFound} | ekar`,
+    title: `404 not found page ${pageNotFound} | ekar`,
   },
 )
 </script>
@@ -16,16 +13,16 @@ useHead(
 <template>
   <main id="NotFound-wrapper" class=" justify-center flex flex-col items-center" :style="{ height: fixVh }">
     <section class=" justify-center flex flex-col items-center">
-      <span class="font-bold text-lg font-serif" p="t-3 b-3">
+      <span class="font-bold text-xl font-serif" p="t-3 b-3">
         Oops!
       </span>
       <div text="center" w="auto" class="flex flex-col">
-        <span p="l2 r2 b2" class="font-serif text-lg">
-          Page  <span class="italic font-bold border-b border-black">
-            {{ $route.params.pageNotFound[0] }}
-          </span> not found
+        <span p="l2 r2 b2" class="font-serif ">
+          Page:  <span class="italic font-bold border-b border-black">
+            {{ pageNotFound[0] }}
+          </span> was not found
         </span>
-        <span class="text-sm font-serif">Please check the path for errors</span>
+        <span class="text-sm font-serif">Please check the path </span>
       </div>
       <span p="t0.5 b0.5 l2 r2" m="t5" text="center white sm" class="bg-black rounded font-serif select-none" @click="router.back()">
         Back
--- utils/pageAnimate.ts
+++ utils/pageAnimate.ts
@@ -16,11 +16,3 @@ export function aboutAnimate() {
     duration: 0.7,
   })
 }
-
-export function NotFoundAnimate() {
-  tl.from('#NotFound-wrapper', {
-    autoAlpha: 0,
-    translateY: 30,
-    duration: 0.7,
-  })
-}
